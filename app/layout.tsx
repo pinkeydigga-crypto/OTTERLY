8125d345672fa6dbbf511901778d1d8360af57ec
harjasdigga <pinkeydigga026@gmail.com>
2026-09-09 20:44:43 +0530

    Fix security headers and optimize Next config

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const nunito = Nunito({
 });
 
 const FAVICON_URL =
-  "https://otsiwrtnkzhrztlpcdjx.supabase.co/storage/v1/object/public/DRAW/Screenshot_9-9-2026_20260_chatgpt.com%20(1).png";
+  "https://otsiwrtnkzhrztlpcdjx.supabase.co/storage/v1/object/public/DRAW/Screenshot_9-9-2026_20260_chatgpt.com-styled%20(1).png";
 
 const OG_IMAGE_URL =
   "https://otsiwrtnkzhrztlpcdjx.supabase.co/storage/v1/object/public/DRAW/OTTO%20LANDING%20PAGE.png";
